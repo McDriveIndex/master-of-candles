9a273f7a4eba7d9364e1eb0e18de97556b3fa140
Inner Fire <innerfire.eth@MacBook-Pro-di-Inner.local>
2026-03-05 12:04:01 +0100

    M10.1: Master breathing animation + eyes pulse (CSS overlay)

--- components/MasterLayer/MasterLayer.tsx
+++ components/MasterLayer/MasterLayer.tsx
@@ -3,32 +3,34 @@ import styles from "./MasterLayer.module.css";
 export default function MasterLayer() {
   return (
     <div className={styles.layer} aria-hidden="true">
-      <div className={styles.hoodWrap}>
+      <div className={styles.masterBreath}>
+        <div className={styles.hoodWrap}>
+          <img
+            src="/master_hood.png"
+            alt="Master hood"
+            draggable={false}
+            className={styles.hood}
+          />
+          <img
+            src="/eyes_glow.png"
+            alt="Master glowing eyes"
+            draggable={false}
+            className={`${styles.eyesGlow} ${styles.eyesPulse}`}
+          />
+        </div>
         <img
-          src="/master_hood.png"
-          alt="Master hood"
+          src="/hand_left.png"
+          alt="Master left hand"
           draggable={false}
-          className={styles.hood}
+          className={styles.handLeft}
         />
         <img
-          src="/eyes_glow.png"
-          alt="Master glowing eyes"
+          src="/hand_right.png"
+          alt="Master right hand"
           draggable={false}
-          className={styles.eyesGlow}
+          className={styles.handRight}
         />
       </div>
-      <img
-        src="/hand_left.png"
-        alt="Master left hand"
-        draggable={false}
-        className={styles.handLeft}
-      />
-      <img
-        src="/hand_right.png"
-        alt="Master right hand"
-        draggable={false}
-        className={styles.handRight}
-      />
     </div>
   );
 }
